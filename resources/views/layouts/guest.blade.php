6185ec30ce54fc48abacb9c4dc04f8cfb5cbf59a
Samwankins <ur843d@gmail.com>
2023-12-18 05:33:39 +0200

    dompdf package added, login and register pages modified

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>دار الفلاح</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+    <style>
+        * {
+            direction: rtl;
+        }
+    </style>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div>
+            <h1 style="color: white">دار الفلاح</h1>
         </div>
-    </body>
+        <div
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\PlanController;
 use App\Http\Controllers\ProfileController;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Dompdf\Dompdf;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('auth.login');
+})->name('welcome');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -37,6 +39,156 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/review-plans', [PlanController::class, 'reviewPlans'])->name('review-plans');
     Route::get('/arp', [PlanController::class, 'addReviewPlan'])->name('add-review-plan');
     Route::post('/crp', [PlanController::class, 'createReviewPlan'])->name('create-review-plan');
+
+
+    Route::get('/print', function () {
+        $request = new object();
+        $request->table = '<table><tr><th>Company<th>Contact<th>Country<tr><td>Alfreds Futterkiste<td>Maria Anders<td>Germany<tr><td>Centro comercial Moctezuma<td>Francisco Chang<td>Mexico<tr><td>Ernst Handel<td>Roland Mendel<td>Austria<tr><td>Island Trading<td>Helen Bennett<td>UK<tr><td>Laughing Bacchus Winecellars<td>Yoshi Tannamuri<td>Canada<tr><td>Magazzini Alimentari Riuniti<td>Giovanni Rovelli<td>Italy</table>';
+        $table = str_replace('\\r|\\n|\\', '', $request->table);
+
+        $document = new Dompdf();
+        $path = 'EDIT3.png';
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data = file_get_contents($path);
+        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        date_default_timezone_set("Africa/Cairo");
+        $html = '<style>
+    
+        @page {
+            margin: 150px 50px 70px 50px;
+        }
+    
+        img {
+            display: block;
+            padding:0;
+            margin:0;
+        }
+        p{
+            margin:0;
+        }
+        h1 {
+            text-align: center;
+            font-size: 22px;
+            margin-bottom: 40px;
+        }
+        h3 {
+            font-size: 18px;
+            font-weight: normal;
+            margin-bottom: 20px;
+        }
+        body{
+          font-family:system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+          margin:5px 10px;
+          padding:5px;
+        }
+        table,tr,td,th,thead,tbody{
+          padding:3px 6px;
+          margin: 5px;
+        }
+        tr:nth-child(even) {background: #FFF}
+        tr:nth-child(odd) {background: #CCC}
+        table{
+          border-collapse: collapse;
+          text-align:left;
+          border-spacing:2px;
+          width:100%;
+        }
+        th{
+          font-weight:bold;
+          border-bottom:1px solid black;
+          background:#FFF;
+          text-align:left;
+        }
+        tr{
+          border-bottom:1px solid black;
+          border-color:inherit;
+          font-size:80%;
+        }
+        #logo-div{
+            margin: 0 auto;
+            width:auto;
+            position: fixed;
+            left: 0px;
+            top: -150px;
+            right: 0px;
+            height: 150px;
+            text-align: center;
+        }
+        #footer{
+            display: block;
+            padding: 10px;
+            bottom: 0px;
+            margin-top:auto;
+            border-top: 1px solid black;
+            position: fixed;
+            left: 0px;
+            bottom: -70px;
+            right: 0px;
+            height: 70px;
+        }
+        .container{
+            margin-top:50px;
+            position: relative;
+            height:auto;
+        }
+        p{
+            margin-bottom:5px;
+        }
+    
+        .footer{
+            display: block;
+            padding: 10px;
+            bottom: 30px;
+            margin-top:auto;
+            position: fixed;
+            left: 0px;
+            right: 0px;
+        }
+    
+        </style>
+    
+        <div id="logo-div">
+            <img src="' . $base64 . '" alt="logo" width="100">
+            <p>AIGPS</p>
+        </div>
+        ' . 'The title from request' . '
+    
+        <div id="footer">
+            <p>Date: ' . date("M d, Y") . ', time: ' . date("h:i A") . '</p>
+        </div>
+    
+        <div class="container">
+    
+            ' .  $table  . '
+            <div style="height:180px;"></div>
+            <div class="footer">
+                <p>Email: aigps.ml@gmail.com</p>
+                <p>Tel: +20 154 2015 467</p>
+                <br>
+                <p>Signature:.......................</p>
+                <p>Remarks:.........................</p>
+            </div>
+        </div>
+    
+        <script type="text/php">
+            $text = "Page {PAGE_NUM} of {PAGE_COUNT}";
+            $font = $fontMetrics->get_font("helvetica", "bold");
+            $size = 11;
+            $color = array(0,0,0);
+            $word_space = 0.0;
+            $char_space = 0.0;
+            $angle = 0.0;
+            $pdf->page_text(500, 800, $text, $font, $size, $color, $word_space, $char_space, $angle);
+    
+        </script>
+        ';
+
+        $document->set_option("isPhpEnabled", true);
+        $document->loadHtml($html);
+        $document->setPaper('A4', 'portrait');
+        $document->render();
+        $document->stream('report.pdf', array("Attachment" => true));
+    });
 });
 
 require __DIR__ . '/auth.php';
